aed0933cb2106b9d6acd96016551495785b5ef7b
willdomkahari <developer.wadu@gmail.com>
2025-03-21 20:24:05 +0200

    FEAT: create product requests

---
+++ services/product/src/main/java/com/waduclay/ecommerce/product/ProductController.java
@@ -0,0 +1,35 @@
+package com.waduclay.ecommerce.product;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+@RestControllerAdvice
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Integer> createProduct(
+            @RequestBody @Valid ProductRequest request
+    ){
+        return ResponseEntity.ok(productService.createProduct(request));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(
+            @RequestBody List<ProductPurchaseRequest> requests
+    ){
+        return ResponseEntity.ok(productService.purchaseProducts(requests));
+    }
+
+    @GetMapping("/{}")
+}
---
+++ services/product/src/main/java/com/waduclay/ecommerce/product/ProductService.java
@@ -0,0 +1,23 @@
+package com.waduclay.ecommerce.product;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    public Integer createProduct(@Valid ProductRequest request) {
+        return null;
+    }
+
+    public List<ProductPurchaseResponse> purchaseProducts(List<ProductPurchaseRequest> requests) {
+        return null;
+    }
+}
