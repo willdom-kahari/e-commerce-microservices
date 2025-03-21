892475391d92a8a8c1618009c90a40ecc4642b03
willdomkahari <developer.wadu@gmail.com>
2025-03-21 11:51:30 +0200

    FEAT: migrations for the entities

--- services/product/src/main/java/com/waduclay/ecommerce/category/Category.java
+++ services/product/src/main/java/com/waduclay/ecommerce/category/Category.java
@@ -18,7 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Integer id;
     private String name;
     private String description;
--- services/product/src/main/java/com/waduclay/ecommerce/product/Product.java
+++ services/product/src/main/java/com/waduclay/ecommerce/product/Product.java
@@ -18,7 +18,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Integer id;
     private String name;
     private String description;
