de5f4e21c290d008eff8f3be09897c5422031d10
willdomkahari <developer.wadu@gmail.com>
2025-03-21 11:02:26 +0200

    FEAT: create entities

---
+++ services/product/src/main/java/com/waduclay/ecommerce/category/Category.java
@@ -0,0 +1,27 @@
+package com.waduclay.ecommerce.category;
+
+
+import com.waduclay.ecommerce.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
---
+++ services/product/src/main/java/com/waduclay/ecommerce/product/Product.java
@@ -0,0 +1,30 @@
+package com.waduclay.ecommerce.product;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import com.waduclay.ecommerce.category.Category;
+
+import java.math.BigDecimal;
+
+/**
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
